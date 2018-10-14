359908c9fc39546f6e7ecafc6767fd82c015f689
Mr-YINHANG <1794053241@qq.com>
2018-10-14 09:38:50 +0800

    demo

---
+++ server.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/14
+ * Time: 9:30
+ */
+$name = $_GET['username'];
+$psd =$_GET['pwd'];
+echo "用户名是：".$name."密码是：".$psd;
